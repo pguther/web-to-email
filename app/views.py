8a2bf30f2671691a520519cf179c6bb5dc6feb68
Philip Guther <philip@guther.com>
2016-06-20 15:11:46 -0700

    added logic to allow skipping directly to results page by inputting the url as a querystring paramater

--- app/views.py
+++ app/views.py
@@ -7,14 +7,22 @@ from utils import PageScraper
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = URLForm()
-    if form.validate_on_submit():
-        # flash('Page Conversion requested for URL="%s"' % form.url.data)
-        url = request.form['url']
+    if 'url' in request.args:
+        url = request.args.get('url')
         scraper = PageScraper()
         article_dictionary = scraper.scrape_page(url)
         return render_template('result.html', banner_image=article_dictionary['banner_image'],
                                title=article_dictionary['title'],
                                content=article_dictionary['content'])
-    return render_template('index.html',
-                           form=form)
+    else:
+        if form.validate_on_submit():
+            # flash('Page Conversion requested for URL="%s"' % form.url.data)
+            url = request.form['url']
+            scraper = PageScraper()
+            article_dictionary = scraper.scrape_page(url)
+            return render_template('result.html', banner_image=article_dictionary['banner_image'],
+                                   title=article_dictionary['title'],
+                                   content=article_dictionary['content'])
+        return render_template('index.html',
+                               form=form)
 
