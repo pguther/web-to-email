622ad3c3ae83b2711f13c8cae325ca295bb227fb
Philip Guther <philip@guther.com>
2016-09-01 10:02:05 -0700

    renamed validators to form_validators

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField
 from wtforms.validators import DataRequired, URL
-from validators import MessagingURl
+from form_validators import MessagingURl
 
 
 class URLForm(Form):
