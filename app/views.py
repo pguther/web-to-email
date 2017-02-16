5d64c0a65e54dcc2d2fd8fd5cf0a8aee1742b202
Philip Guther <philip@guther.com>
2017-02-16 11:55:32 -0800

    added some very basic error page templates

--- app/views.py
+++ app/views.py
@@ -32,3 +32,16 @@ def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
             flash(error)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
