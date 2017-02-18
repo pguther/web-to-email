ccc0b575979815ea5cc8fa48afdc20c91805d2d6
Rob Knight <raknight@ucsc.edu>
2017-02-18 10:59:03 -0800

    Add Slack notifications for 403, 404, or 500 errors

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app
 from forms import URLForm
 from utils import MessagingScraper
+import requests
+import os
 
 
 @app.route('/', methods=['GET', ])
@@ -33,15 +35,24 @@ def flash_errors(form):
         for error in errors:
             flash(error)
 
+def send_error_to_slack(error, url):
+    slack_url = os.environ.get('SLACK_WEBHOOK_URL')
+    error_message = "Error in the *Web-to-Email* tool: %(error)s: \nURL: %(url)s" % locals()
+    message_payload = {'text': error_message}
+    requests.post(slack_url, json=message_payload)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
+    send_error_to_slack(e, request.url)
     return render_template('404.html'), 404
 
 @app.errorhandler(403)
 def page_not_found(e):
+    send_error_to_slack(e, request.url)
     return render_template('403.html'), 403
 
 @app.errorhandler(500)
 def page_not_found(e):
+    send_error_to_slack(e, request.url)
     return render_template('500.html'), 500
