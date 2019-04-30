107b2567dd8a2ec8b34d6e6285636cfd32a9e8db
LuckyLuke007 <trung@scgstudio.com>
2019-04-30 14:22:16 -0700

    errors

--- app/utils.py
+++ app/utils.py
@@ -7,7 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from premailer import Premailer
-from errors import ErrorCategory, ErrorType
+from .errors import ErrorCategory, ErrorType
 
 
 class ContentNotHTMLException(Exception):
