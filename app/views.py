64bdd6eb30404eec8ec72664c3c27dd3060c158d
LuckyLuke007 <trung@scgstudio.com>
2019-04-30 11:08:50 -0700

    form_validators to .form_validators

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from .forms import URLForm
+from forms import URLForm
 from utils import MessagingScraper
 import requests
 import os
