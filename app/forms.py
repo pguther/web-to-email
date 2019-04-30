e2569eeb65d6f3b6fd2069a248c227f60244447e
LuckyLuke007 <trung@scgstudio.com>
2019-04-30 09:17:12 -0700

    Add back message, forms.py FlaskForm to Form and WTForms change requirements 2.1 to 2.2.1

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import StringField
 from wtforms.validators import DataRequired, URL
 from form_validators import MessagingURl
