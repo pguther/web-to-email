64ab9dd5d85e39f13b4e8dd45185112f42b93838
LuckyLuke007 <trung@scgstudio.com>
2019-05-07 14:36:14 -0700

    remove . from form_validators

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, URL
-from .form_validators import MessagingURl
+from form_validators import MessagingURl
 
 
 class URLForm(FlaskForm):
